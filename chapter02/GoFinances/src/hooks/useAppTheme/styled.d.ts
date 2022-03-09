17d7d98805a7ef0bb67171fb144eeb1f40addaf9
augustojaml <jamonteirolima@gmail.com>
2022-03-09 16:04:34 -0300

    Chapter II - Deletado projeto para recriação | Começando pelo theme, navigator

--- chapter02/GoFinances/src/hooks/useAppTheme/styled.d.ts
+++
@@ -1,8 +0,0 @@
-import 'styled-components';
-import { theme } from './theme';
-
-declare module 'styled-components' {
-  const Types = theme.dark || theme.light;
-  type ThemeType = typeof Types;
-  export interface DefaultTheme extends ThemeType {}
-}
